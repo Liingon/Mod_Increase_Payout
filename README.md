72884306fe195f47bdf604c5842422a4d4812a91
Jesper Ekstrand <jesper@level15.se>
2020-01-05 20:29:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Increase payout mod for Project Hospital
+Increases payout for all Diagnoses by a modifier. Includes python script to generate modified files.
+
+Tested on version 1.1.17753
+
+## Using the mod
+Put the mod folder in `ProjectHospital_Data\StreamingAssets\Addons` in the game folder.
+
+## Requirements for script
+- Project Hospital installed
+- Python 3
+
+## Running the script
+- If the game is not installed from Steam to default folder, modify `gamePath` accordingly.
+- Run the script with `python generate_xml.py`
---
+++ generate_xml.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os
+import xml.etree.ElementTree as ET
+
+gamePath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Project Hospital"
+diagnosePath = "\\ProjectHospital_Data\\StreamingAssets\\Database\\Diagnoses"
+path = gamePath + diagnosePath
+modifier = 1.5
+
+# Loop through all Dignoses files
+def loopFiles( path ):
+    for file in os.listdir(path):
+        if file.startswith("Diagnoses") and file.endswith(".xml"):
+            parseFile(os.path.join(path, file))
+
+# Find all instances of GameDBMedicalCondition, increase InsurancePayment and remove everything else, then print to output file
+def parseFile( file ):
+    filename = os.path.basename(file)
+    tree = ET.parse( file )
+    root = tree.getroot()
+    
+    for elem in root.iter('GameDBMedicalCondition'):
+        InsurancePayment = elem.find('InsurancePayment')
+        InsurancePayment.text = modify(InsurancePayment.text)
+    
+    tree.write("Database/Mod" + filename)
+
+# Modify a value
+def modify( value ):
+    value = int(value)
+    value = value * modifier
+    return str(int(value))
+
+loopFiles( path )
